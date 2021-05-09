f42867e42141efce4d9540e901f02abae87ebc57
Bárbara Alfaro <barbaraabaraqui@gmail.com>
2021-05-09 12:35:50 -0400

    footer cambios

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './Navbar.js';
+import Footer from './Footer.js';
 
 function App() {
   return (
     <div>
        
         <Navbar/>
+        <Footer/>
     </div>
   );
 }
