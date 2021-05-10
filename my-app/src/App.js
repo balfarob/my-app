1c62d08d491e08e34bdca68b4664ecb4ec8730e1
Bárbara Alfaro <barbaraabaraqui@gmail.com>
2021-05-09 21:01:42 -0400

    utilizando browserRouter

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,14 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './Navbar.js';
 import Footer from './Footer.js';
+import { BrowserRouter, Router, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
+      <div>
        
         <Navbar/>
+         <Switch>
+           <Router path="/contacto">
+             Contactanos
+           </Router>
+         </Switch>
         <Footer/>
-    </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
